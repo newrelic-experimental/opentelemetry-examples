ad53aa651a62f5f3ff4e335dcb117a5848bc1416
Brad Schmitt <bpschmitt@users.noreply.github.com>
2025-03-11 14:27:02 -0500

    Update setVars.sh
    add region

--- workshop/setVars.sh
+++ workshop/setVars.sh
@@ -3,11 +3,12 @@
 read -p "Please enter your New Relic Account ID: " ACCOUNT_ID
 read -p "Please enter your New Relic License Key: " LICENSE_KEY
 read -p "Please enter your New Relic User API Key: " USER_API_KEY
+read -p "Please enter your New Relic Region (US/EU): " REGION
 
 echo "export NEW_RELIC_LICENSE_KEY=$LICENSE_KEY" >> ~/.bashrc
 echo "export NEW_RELIC_API_KEY=$USER_API_KEY" >> ~/.bashrc
 echo "export NEW_RELIC_ACCOUNT_ID=$ACCOUNT_ID" >> ~/.bashrc
-echo "export NEW_RELIC_REGION=US" >> ~/.bashrc
+echo "export NEW_RELIC_REGION=$REGION" >> ~/.bashrc
 echo ""
 echo "Now run the following command to refresh your environment:"
 echo ""
